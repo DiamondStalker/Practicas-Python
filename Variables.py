ffed1f9fc4bfc7503150f2f91d9f79f7f89c58f2
DiamondStalker <cmoreno981@gmail.com>
2024-07-04 12:39:36 -0500

    Archivo de variables y diccionarios

---
+++ Variables.py
@@ -0,0 +1,6 @@
+# Para definir variables en Python no se le define sino que se ele asigna el valor directamente
+
+a = 5
+
+## Forma sencilla de imprimir una variable
+print(a) 
---
+++ diccionarios.py
@@ -0,0 +1,13 @@
+""" 
+    Un diccionario es un conjunto de informacion estructurada con orden y sentido 
+    por ejemplo si quiero almacenar informacion de un usuario como:
+        - Nombre
+        - Edad
+        - Telefono
+    La estructura queda como:
+"""
+
+usuario = {
+    "Nombre": "Carlos",
+    "Edad": 10
+}
