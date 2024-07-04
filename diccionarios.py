1a5c6c6e47e715a5abf4fd4dee37a18327fa50bb
DiamondStalker <cmoreno981@gmail.com>
2024-07-04 13:21:21 -0500

    Some changes

--- diccionarios.py
+++ diccionarios.py
@@ -10,6 +10,21 @@
 usuario = {
     "Nombre": "Carlos",
     "Edad": 10,
-    "Deporte":{}
+    "Materias": {
+        "Matematicas" : {
+            "Notas": [5,5,5,1,1],
+            "NotaFinal":0
+        }
+    }
 }
 
+suma = 0
+for i in usuario["Materias"]["Matematicas"]["Notas"]:
+    suma+= i
+
+print(usuario)
+
+
+usuario["Materias"]["Matematicas"]["NotaFinal"] = suma/len(usuario["Materias"]["Matematicas"]["Notas"]) # Asignar un valor a un diccionario de datos
+
+print(usuario)
